36d7901289084a28ab5b2401ecf3753009f1fe5e
Jose Garcia <jogarcia@tuta.io>
2022-05-04 14:37:58 -0400

    Move history to SectionHistory

---
+++ components/SectionHistory.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="istorija container">
+    <div>
+      <h1>Istorija</h1>
+      <p>
+        Žmonijai išnykus, mistiškai pradeda formuotis naujas pasaulis kitoje
+        visatos erdvėje. Ten užsimezgusi gyvybė buvo gana primityvi. Ne, tai
+        nebuvo vienaląsčiai organizmai nesugebantys mąstyti daugiau negu to
+        reikia išgyvenimui, tačiau apie savo planetos pavadinimą jie net
+        nemąstė, jiems ji buvo mistiška <br />
+        ir neištirta, dėl to visi ją vadino paprastai -
+        <span style="font-weight: bold">Mistinis pasaulis</span>.<br />
+        Šis pasaulis susidarė iš 4 bendruomenių-rasių išsibarsčiusių jame. Šios
+        rasės gyveno atskiruose žemynuose ir puoselėjo savo miestelius.
+        Apsilankius vienos ar kitos rasės gyvenamosiose vietose, galima buvo
+        pamatyti unikalius, išskirtinius statinius atitinkančius rasės
+        tradicijas, gabumus ir gyvenimo būdą. Mistinio pasaulio gyventojai
+        turėjo tikslą būti geriausiems ir stengėsi bendradarbiauti vieni su
+        kitais tam, kad kiekvieni iš jų sukurtų jaukų, įdomų, savo neįtikėtiną
+        rasės miestą. Taigi, klestinčių rasių pavadinimai: Haflingai, Tabaksiai,
+        Uritonai ir Minotaurai.
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'SectionHistory',
+}
+</script>
+
+<style lang="scss" scoped>
+.istorija {
+  display: flex;
+  justify-content: center;
+  text-align: center;
+}
+
+div {
+  max-width: 800px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -23,29 +23,7 @@
       </div>
     </section>
 
-    <section class="istorija container">
-      <div>
-        <h1>Istorija</h1>
-        <p>
-          Žmonijai išnykus, mistiškai pradeda formuotis naujas pasaulis kitoje
-          visatos erdvėje. Ten užsimezgusi gyvybė buvo gana primityvi. Ne, tai
-          nebuvo vienaląsčiai organizmai nesugebantys mąstyti daugiau negu to
-          reikia išgyvenimui, tačiau apie savo planetos pavadinimą jie net
-          nemąstė, jiems ji buvo mistiška <br />
-          ir neištirta, dėl to visi ją vadino paprastai -
-          <span style="font-weight: bold">Mistinis pasaulis</span>.<br />
-          Šis pasaulis susidarė iš 4 bendruomenių-rasių išsibarsčiusių jame.
-          Šios rasės gyveno atskiruose žemynuose ir puoselėjo savo miestelius.
-          Apsilankius vienos ar kitos rasės gyvenamosiose vietose, galima buvo
-          pamatyti unikalius, išskirtinius statinius atitinkančius rasės
-          tradicijas, gabumus ir gyvenimo būdą. Mistinio pasaulio gyventojai
-          turėjo tikslą būti geriausiems ir stengėsi bendradarbiauti vieni su
-          kitais tam, kad kiekvieni iš jų sukurtų jaukų, įdomų, savo neįtikėtiną
-          rasės miestą. Taigi, klestinčių rasių pavadinimai: Haflingai,
-          Tabaksiai, Uritonai ir Minotaurai.
-        </p>
-      </div>
-    </section>
+    <section-history></section-history>
 
     <section-races-carousel></section-races-carousel>
 
@@ -141,12 +119,8 @@
 </template>
 
 <script>
-import SectionRacesCarousel from '@/components/SectionRacesCarousel.vue'
-import AppFooter from '@/components/AppFooter.vue'
-
 export default {
   name: 'IndexPage',
-  components: { SectionRacesCarousel, AppFooter },
 }
 </script>
 
@@ -255,14 +229,6 @@ h5 {
   font-size: 20px;
   margin-top: 0px;
   margin-bottom: 2%;
-}
-
-/* istorija */
-.istorija {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
 }
 
 .servers__title-container {
