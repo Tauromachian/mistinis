7fab34c954b35b35e69c24bc2caced7b84195b11
Jose Garcia <jogarcia@tuta.io>
2022-05-04 21:51:52 -0400

    Move Hero to its own component

---
+++ components/SectionHero.vue
@@ -0,0 +1,80 @@
+<template>
+  <section class="hero">
+    <header>
+      <div class="pagrindinisbg">
+        <nav>
+          <ul>
+            <li><a href="#">Pagrindinis</a></li>
+            <li><a href="#">Istorija</a></li>
+            <li><a href="#">Rasės</a></li>
+            <li><a href="#">Galerija</a></li>
+            <li><a href="#">Kontaktai</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <div class="pagrindinis">
+      <img id="logo" src="/images/logo.png" alt="" />
+      <h5>
+        Serverje žaidžia: <span style="font-weight: bold">25 žaidėjai</span>
+      </h5>
+      <div id="discord"><button>Discord</button></div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'SectionHero',
+}
+</script>
+
+<style lang="scss" scoped>
+.hero {
+  padding-top: 0em;
+  padding-bottom: 0em;
+  height: 100vh;
+  background: url(/images/test.jpg);
+  background-repeat: no-repeat;
+  background-size: 100% 100vh;
+}
+
+nav {
+  text-align: center;
+}
+
+nav ul {
+  display: flex;
+  justify-content: center;
+}
+
+nav ul li {
+  text-align: center;
+  margin-top: 50px;
+  list-style-type: none;
+  width: 130px;
+}
+
+nav ul li a {
+  text-decoration: none;
+  color: inherit;
+}
+
+#logo {
+  display: block;
+  margin: 0 auto;
+  width: 30%;
+}
+
+#discord {
+  text-align: center;
+}
+
+h5 {
+  text-align: center;
+  font-weight: normal;
+  font-size: 20px;
+  margin-top: 0px;
+  margin-bottom: 2%;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,6 @@
 <template>
   <div>
-    <section class="hero">
-      <header>
-        <div class="pagrindinisbg">
-          <nav>
-            <ul>
-              <li><a href="#">Pagrindinis</a></li>
-              <li><a href="#">Istorija</a></li>
-              <li><a href="#">Rasės</a></li>
-              <li><a href="#">Galerija</a></li>
-              <li><a href="#">Kontaktai</a></li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-      <div class="pagrindinis">
-        <img id="logo" src="/images/logo.png" alt="" />
-        <h5>
-          Serverje žaidžia: <span style="font-weight: bold">25 žaidėjai</span>
-        </h5>
-        <div id="discord"><button>Discord</button></div>
-      </div>
-    </section>
+    <section-hero></section-hero>
 
     <section-history></section-history>
 
@@ -98,53 +77,6 @@ section {
 }
 
 /* Hero section */
-.hero {
-  padding-top: 0em;
-  padding-bottom: 0em;
-  height: 100vh;
-  background: url(/images/test.jpg);
-  background-repeat: no-repeat;
-  background-size: 100% 100vh;
-}
-
-nav {
-  text-align: center;
-}
-
-nav ul {
-  display: flex;
-  justify-content: center;
-}
-
-nav ul li {
-  text-align: center;
-  margin-top: 50px;
-  list-style-type: none;
-  width: 130px;
-}
-
-nav ul li a {
-  text-decoration: none;
-  color: inherit;
-}
-
-#logo {
-  display: block;
-  margin: 0 auto;
-  width: 30%;
-}
-
-#discord {
-  text-align: center;
-}
-
-h5 {
-  text-align: center;
-  font-weight: normal;
-  font-size: 20px;
-  margin-top: 0px;
-  margin-bottom: 2%;
-}
 
 .servers__title-container {
   text-align: center;
