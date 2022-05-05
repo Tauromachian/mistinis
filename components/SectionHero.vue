6ea0f26508e736c91fd35c130019c22c9fa27111
Jose Garcia <jogarcia@tuta.io>
2022-05-04 22:22:47 -0400

    Set basic menu responsivity

--- components/SectionHero.vue
+++ components/SectionHero.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="hero">
     <header>
-      <nav>
+      <nav v-if="!isScreenSmall">
         <ul>
           <li><a href="#">Pagrindinis</a></li>
           <li><a href="#">Istorija</a></li>
@@ -10,6 +10,28 @@
           <li><a href="#">Kontaktai</a></li>
         </ul>
       </nav>
+      <div v-else>
+        <button class="modal-open-button" @click="openModal">
+          <i class="fa-solid fa-bars"></i>
+        </button>
+        <div id="modal">
+          <nav class="modal-nav">
+            <ul class="modal-links">
+              <li><a href="#">Pagrindinis</a></li>
+              <li><a href="#">Istorija</a></li>
+              <li><a href="#">Rasės</a></li>
+              <li><a href="#">Galerija</a></li>
+              <li><a href="#">Kontaktai</a></li>
+            </ul>
+          </nav>
+          <button
+            class="modal-close-button position-absolute"
+            @click="closeModal"
+          >
+            <i class="fa-solid fa-xmark"></i>
+          </button>
+        </div>
+      </div>
     </header>
     <div class="pagrindinis">
       <img id="logo" src="/images/logo.png" alt="" />
@@ -24,10 +46,109 @@
 <script>
 export default {
   name: 'SectionHero',
+  data() {
+    return {
+      modal: {},
+      body: {},
+      width: '',
+    }
+  },
+  computed: {
+    isScreenSmall() {
+      return this.width < 991
+    },
+    path() {
+      return this.$nuxt.$route.name
+    },
+    logo() {
+      const routeName = this.$nuxt.$route.name
+      if (routeName === 'repair' || routeName === 'service') {
+        return '/JTRS-min.png'
+      }
+      return '/johnson-logo-min.png'
+    },
+  },
+  mounted() {
+    this.modal = document.getElementById('modal')
+    this.body = document.body
+    this.closeModal()
+    this.width = window.innerWidth
+    window.addEventListener('resize', () => {
+      this.width = window.innerWidth
+    })
+  },
+
+  methods: {
+    openModal() {
+      this.modal.style.display = 'block'
+      this.body.style.overflow = 'hidden'
+    },
+    closeModal() {
+      this.modal.style.display = 'none'
+      this.body.style.overflow = 'unset'
+    },
+  },
 }
 </script>
 
 <style lang="scss" scoped>
+button {
+  cursor: pointer;
+}
+
+.hidden {
+  display: none;
+}
+
+#modal {
+  display: none;
+  position: fixed;
+  background-color: rgb(14, 13, 13);
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: 1000;
+}
+
+.modal-nav {
+  height: 100%;
+}
+
+.modal-links {
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.modal-open-button {
+  position: absolute;
+  top: 10px;
+  right: 20px;
+  font-size: 1.5rem;
+  width: fit-content;
+  padding: 1em;
+  display: flex;
+  align-content: center;
+  background-color: transparent;
+}
+
+.modal-close-button {
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  font-size: 1.5rem;
+  background-color: transparent;
+}
+
+.modal-close-button > i {
+  color: white;
+}
+
 .hero {
   padding-top: 0em;
   padding-bottom: 0em;
