7e7ed9d79ed42263ec46372ce95775742e9566e8
Jose Garcia <jogarcia@tuta.io>
2022-05-06 18:21:48 -0400

    Create section server

---
+++ components/SectionServer.vue
@@ -0,0 +1,16 @@
+<template>
+  <iframe src="http://95.217.88.120:8124/" frameborder="0"></iframe>
+</template>
+
+<script>
+export default {
+  name: 'SectionServer',
+}
+</script>
+
+<style lang="scss" scoped>
+iframe {
+  width: 100%;
+  height: calc(100vh - 140px);
+}
+</style>
