769a6ddeb698ed13a6ea4eab1414157fcb2c191c
Jose Garcia <jogarcia@tuta.io>
2022-05-11 17:53:41 -0400

    Scope footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -57,117 +57,116 @@ footer {
       background-color: rgb(0, 0, 0, 0);
     }
   }
-}
-
-.footer-content {
-  display: flex;
-  position: relative;
-  justify-content: center;
-  text-align: center;
-
-  @media screen and(min-width: 800px) {
-    text-align: left;
-    justify-content: left;
-  }
-
-  h2 {
-    font-size: 2.5rem;
-    font-weight: bold;
-  }
-
-  @media screen and(min-width: 800px) {
-    h2 {
-      font-size: 3.2rem;
-      font-weight: bold;
-    }
-  }
-
-  @media screen and(min-width: 1500px) {
-    h2 {
-      font-size: 4rem;
-      font-weight: bold;
-    }
-  }
-
-  h3 {
-    color: #ad00ff;
-    font-weight: 500;
-    font-size: 2rem;
-    margin-bottom: 0.8em;
-  }
-
-  @media screen and(min-width: 800px) {
-    h3 {
-      color: #ad00ff;
-      font-weight: 500;
-      font-size: 2.5rem;
-      margin-bottom: 0.8em;
-    }
-  }
-
-  @media screen and(min-width: 1500px) {
-    h3 {
-      color: #ad00ff;
-      font-weight: 500;
-      font-size: 3.2rem;
-      margin-bottom: 0.8em;
-    }
-  }
-
-  ul {
-    list-style: none;
-  }
-
-  li {
-    margin-top: 1em;
-  }
-
-  i {
-    font-size: 1.4rem;
-  }
-
-  button {
-    margin-top: 2em;
-  }
-
-  .footer-content-text {
+  .footer-content {
+    display: flex;
     position: relative;
-    z-index: 40;
-    padding-top: 10em;
-    padding-bottom: 10em;
-    div {
-      flex-basis: 50%;
-    }
-  }
+    justify-content: center;
+    text-align: center;
 
-  @media screen and(min-width: 800px) {
+    @media screen and(min-width: 800px) {
+      text-align: left;
+      justify-content: left;
+    }
+
+    h2 {
+      font-size: 2.5rem;
+      font-weight: bold;
+    }
+
+    @media screen and(min-width: 800px) {
+      h2 {
+        font-size: 3.2rem;
+        font-weight: bold;
+      }
+    }
+
+    @media screen and(min-width: 1500px) {
+      h2 {
+        font-size: 4rem;
+        font-weight: bold;
+      }
+    }
+
+    h3 {
+      color: #ad00ff;
+      font-weight: 500;
+      font-size: 2rem;
+      margin-bottom: 0.8em;
+    }
+
+    @media screen and(min-width: 800px) {
+      h3 {
+        color: #ad00ff;
+        font-weight: 500;
+        font-size: 2.5rem;
+        margin-bottom: 0.8em;
+      }
+    }
+
+    @media screen and(min-width: 1500px) {
+      h3 {
+        color: #ad00ff;
+        font-weight: 500;
+        font-size: 3.2rem;
+        margin-bottom: 0.8em;
+      }
+    }
+
+    ul {
+      list-style: none;
+    }
+
+    li {
+      margin-top: 1em;
+    }
+
+    i {
+      font-size: 1.4rem;
+    }
+
+    button {
+      margin-top: 2em;
+    }
+
     .footer-content-text {
-      padding: 5em 0em 20em 0em;
+      position: relative;
+      z-index: 40;
+      padding-top: 10em;
+      padding-bottom: 10em;
+      div {
+        flex-basis: 50%;
+      }
+    }
+
+    @media screen and(min-width: 800px) {
+      .footer-content-text {
+        padding: 5em 0em 20em 0em;
+      }
+    }
+
+    @media screen and(min-width: 1500px) {
+      .footer-content-text {
+        padding: 10em 0em 30em 0em;
+      }
+    }
+
+    img {
+      z-index: 0;
+      width: auto;
+      height: 100%;
+      position: absolute;
+      right: 0;
     }
   }
 
-  @media screen and(min-width: 1500px) {
-    .footer-content-text {
-      padding: 10em 0em 30em 0em;
-    }
+  .copyright {
+    padding: 15px;
+    font-style: normal;
+    font-size: 17px;
+    line-height: 29px;
+    text-align: center;
+    color: white;
+    background: black;
   }
-
-  img {
-    z-index: 0;
-    width: auto;
-    height: 100%;
-    position: absolute;
-    right: 0;
-  }
-}
-
-.copyright {
-  padding: 15px;
-  font-style: normal;
-  font-size: 17px;
-  line-height: 29px;
-  text-align: center;
-  color: white;
-  background: black;
 }
 </style>
